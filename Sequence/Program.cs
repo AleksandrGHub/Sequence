ec938d624db0edb31c7c92fbbc2018090d9f19aa
AleksandrGHub <bulanov.shura@gmail.com>
2024-05-29 22:08:48 +0300

    Добавьте файлы проекта.

---
+++ Sequence/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 103;
+            int summandNumber = 7;
+
+            for (int minNumber = 5; minNumber <= maxNumber; minNumber += summandNumber)
+            {
+                Console.WriteLine(minNumber);
+            }
+        }
+    }
+}
